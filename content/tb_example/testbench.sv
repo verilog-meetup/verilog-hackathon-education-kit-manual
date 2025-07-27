6dfb6665dd3b3cf3a7dffc15e38a13e1b03c27a5
Yuri Panchul <yuri@panchul.com>
2025-07-26 20:58:34 -0700

    Split testbench on simple and compilicated

--- content/tb_example/testbench.sv
+++ content/tb_example/testbench.sv
@@ -1,3 +1,5 @@
+/*
+
 module full_adder_testbench;
 
     timeunit      1ns;
@@ -32,8 +34,8 @@ module full_adder_testbench;
 
         // Logging the result
 
-        if (~ (sum === 1'b0 & carry_out == 1'b1))
-            $display ("Unexpected result");
+        if (~ (sum === 1'b0 & carry_out === 1'b1))
+            $display ("Unexpected result: %b", { carry_out, sum });
 
         // A random test with better logging and self-checking
 
@@ -62,6 +64,41 @@ module full_adder_testbench;
                 $finish;
             end
         end
+
+        $finish;
+    end
+
+endmodule
+
+*/
+
+module full_adder_testbench;
+
+    timeunit      1ns;
+    timeprecision 1ps;
+
+    logic a, b, carry_in, sum, carry_out;
+
+    full_adder dut_instance
+    (
+        .a (a), .b (b), .carry_in  (carry_in),
+        .sum (sum),     .carry_out (carry_out)
+    );
+
+    initial
+    begin
+        $dumpvars;
+
+        a        = 1'b0;
+        b        = 1'b1;
+        carry_in = 1'b1;
+
+        # 10
+
+        if (sum === 1'b0 & carry_out === 1'b1)
+            $display ("PASS");
+        else
+            $display ("FAIL: Unexpected result: %b", { carry_out, sum });
 
         $finish;
     end
